5bb139a78c9cc0ce7025204dabd3318b9cda1e5c
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2025-01-26 14:49:31 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 37208 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,31 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println(findLastNumber(n));
-    }
-    public static Integer findLastNumber(int n){
-        Queue<Integer> arr = new ArrayDeque<>();
-        for(int i = 0; i < n; i++){ // 카드 배열 초기화
-            arr.offer(i+1);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine()); // 카드의 갯수
+
+        Queue<Integer> card = new ArrayDeque<>(); // 카드 큐
+        for(int i = 1; i < n + 1; i++){
+            card.add(i);
         }
-        while(arr.size() > 1){ // 한 장이 남을때까지 반복.
-            arr.poll(); // 제일 위에 있는 카드를 바닥에 버린다.
-            arr.offer(arr.poll()); // 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+        // 남은 카드가 1장이 되기 전까지 버리기
+        while(card.size() != 1){
+            card.poll();
+            // 버린 이후에 1장이 아니면 1번째 카드를 큐 제일 뒤로 붙이기
+            if(card.size() != 1){
+                int attach = card.poll();
+                card.add(attach);
+            }
         }
-        return arr.poll(); // 마지막 한 장 반환
+        // 마지막 남은 1장의 카드 출력
+        bw.write(String.valueOf(card.poll()));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
