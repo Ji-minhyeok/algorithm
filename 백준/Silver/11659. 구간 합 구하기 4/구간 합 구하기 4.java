1857cf8f24c5c122aa092124e722bc88f3efc0c7
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2025-06-03 00:06:26 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 532 ms, Memory: 58040 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,45 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        input = br.readLine();
-        st = new StringTokenizer(input);
+        st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[n]; // 일반 배열
-        for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] sumArr = new int[n+1];
 
-        int[] sumArr = new int[n]; // arr배열의 합배열
-        sumArr[0] = arr[0]; // 인덱스 0 초기화
-        for(int i = 1; i < n; i++){ // 합배열 구성하기
-            sumArr[i] = sumArr[i - 1] + arr[i];
+        // 합배열 생성
+        for(int i = 1; i <= n; i++){
+            sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
         }
 
         for(int i = 0; i < m; i++){
-            input = br.readLine();
-            st = new StringTokenizer(input);
-            int first = Integer.parseInt(st.nextToken()); // 구간의 첫 인덱스
-            int last = Integer.parseInt(st.nextToken()); // 구간의 마지막 인덱스
-
-            // 마지막 줄이 아닐 경우 개행문자 추가
-            if (i == m - 1) {
-                bw.write(String.valueOf(panelCalculator(sumArr, first, last)));
-            } else {
-                bw.write(String.valueOf(panelCalculator(sumArr, first, last)) + '\n');
-            }
+            st = new StringTokenizer(br.readLine());
+            int startNum = Integer.parseInt(st.nextToken());
+            int endNum = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(sumArr[endNum]-sumArr[startNum-1])+"\n");
         }
         bw.flush();
-    }
-
-    private static int panelCalculator(int[] sumArr, int first, int last) {
-        if(first == 1){
-            return sumArr[last - 1];
-        }else return sumArr[last-1] - sumArr[first-2];
+        br.close();
+        bw.close();
     }
 }
