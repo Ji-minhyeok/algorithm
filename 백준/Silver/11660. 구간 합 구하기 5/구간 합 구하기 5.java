ad1d998ec3eaca7845da6393106b62712bcb2794
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2025-06-10 23:47:55 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 800 ms, Memory: 129260 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,51 +1,45 @@
-
 import java.io.*;
 import java.util.StringTokenizer;
-// 1-based index 적용해 가독성 향상
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n+1][n+1]; // n * n 배열 생성
-        for (int i = 1; i <= n; i++) { // n번 반복
-            input = br.readLine();  // 한 줄씩 입력받음
-            st = new StringTokenizer(input);
+        int[][] arr = new int[n+1][n+1]; //N+1 형태 배열
+        int[][] sumArr = new int[n+1][n+1]; //N+1 형태 배열
 
-            for (int j = 1; j <= n; j++) { // i 번째 줄 내용 차례대로 배열에 저장
+        // 배열 초기화
+        for(int i = 1; i <= n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] sumArr = new int[n+1][n+1]; // 배열의 2차원 구간 합배열 n * n 생성
-        for (int x = 1; x <= n; x++) { // 나머지 구간 채우기
-            for (int y = 1; y <= n; y++) { // n번 반복
-                sumArr[x][y] = sumArr[x-1][y] + sumArr[x][y-1] - sumArr[x-1][y-1] + arr[x][y];
+        // 합배열 초기화
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + arr[i][j];
             }
         }
 
-        for (int i = 0; i < m; i++) { // m번 반복 (합을 구하는 횟수)
-            input = br.readLine();  // 한 줄씩 입력받음
-            st = new StringTokenizer(input);
-            //x, y좌표 받기
-            int x1 = Integer.parseInt(st.nextToken()); // 행
-            int y1 = Integer.parseInt(st.nextToken()); // 열
-            int x2 = Integer.parseInt(st.nextToken()); // 행
-            int y2 = Integer.parseInt(st.nextToken()); // 열
-            int result = 0;
+        // 구간합 계산
+        for(int i = 0; i < m; i++){
+            // 구간합 질의
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
-
-            result += sumArr[x2][y2] - sumArr[x2][y1 - 1] - sumArr[x1 - 1][y2] + sumArr[x1 - 1][y1 - 1];
-
-            if (i == m - 1) { // 마지막 문자열 처리
-                bw.write(String.valueOf(result));
-            } else bw.write(String.valueOf(result) + '\n');
+            // 구간합 계산
+            int result = sumArr[x2][y2] - sumArr[x2][y1-1] - sumArr[x1-1][y2] + sumArr[x1-1][y1-1];
+            bw.write(String.valueOf(result)+"\n");
         }
         bw.flush();
         bw.close();
