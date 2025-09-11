e8fa6dff50ad78513c795fcee0b2be96754de8fb
JiMinhyeok <87597610+Ji-minhyeok@users.noreply.github.com>
2025-09-11 18:18:55 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 26572 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,54 +7,38 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine()); // 수열의 길이
-        Stack<Integer> stack = new Stack<Integer>(); // 검증용 스택
-        int num = 1; // 현재 스택 push 값 (오름차순)
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++){ // n 만큼 반복
-            //입력받은 수열의 N번째 값
-            int inputNum = Integer.parseInt(br.readLine());
+        // 정답 비교 배열 초기화
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-            if(inputNum == num){ // N번째 값이 현재 스택 push 값이면
-                // num을 push, pop
-                stack.push(num);
+        int currentNum = 1;
+        Stack<Integer> stack = new Stack<>();
+
+        // 정답 배열 순회
+        for(int i = 0; i < n; i++){
+            int target = arr[i];
+            // 타겟 도달까지 push
+            while(target >= currentNum){
+                stack.push(currentNum);
                 sb.append("+\n");
+                currentNum ++;
+            }
+            // 타겟과 같으면 pop
+            if(stack.peek() == target){
                 stack.pop();
                 sb.append("-\n");
-                //num에 +1 연산 (다음 스택 push 값)
-                num++;
-            }else if(inputNum > num){ // N번째 값이 현재 스택 push 값보다 크다면
-
-                // num이 inputNum과 같아지기 전까지 push
-                while(inputNum > num){
-                    stack.push(num);
-                    sb.append("+\n");
-                    //num에 +1 연산 (다음 스택 push 값)
-                    num++;
-                } // inputNum과 num 값이 같아지면
-                // num을 push, pop
-                stack.push(num);
-                sb.append("+\n");
-                stack.pop();
-                sb.append("-\n");
-                //num에 +1 연산 (다음 스택 push 값)
-                num++;
-            }else{ //N번째 값이 현재 스택 push 값보다 작다면
-                // top값이 inputNum과 다르다면 스택으로 구현 불가
-                if(inputNum != stack.peek()){
-                    sb.setLength(0);
-                    sb.append("NO");
-                    bw.write(sb.toString());
-                    bw.flush();
-                    bw.close();
-                    br.close();
-                    return; // 강제 종료
-                }else{
-                    stack.pop();
-                    sb.append("-\n");
-                }
+            }// 타겟과 다르다면 불가 조건
+            else{
+                sb.setLength(0);
+                sb.append("NO");
+                break;
             }
         }
+
         // 출력
         bw.write(sb.toString());
         bw.flush();
